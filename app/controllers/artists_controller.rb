76343df950ea4ead8d6f4924aeef7807485bc22e
tomomi ogino <s4tm83@gmail.com>
2021-02-13 02:13:03 +0900

    add: rspotify設定、indexページの検索フォーム実装

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,12 @@
 class ArtistsController < ApplicationController
+  require 'rspotify'
+  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
+
   def index
+    @artists = Artist.all
+    if params[:search].present?
+      @searchartists = RSpotify::Artist.search(params[:search], market: 'JP')
+    end
   end
 
   def new
