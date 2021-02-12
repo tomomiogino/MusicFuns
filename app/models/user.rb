1038138c16d429717bee4c3c1f9f13b85949e6bc
tomomi ogino <s4tm83@gmail.com>
2021-02-12 14:18:39 +0900

    add: userモデル バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  with_options presence: true do
+    validates :name, length: { maximum: 20 }
+    validates :email, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+    validates :password, length: {minimum: 6}, on: :create
+  end
+
+  validates :profile_comment, length: {maximum: 255}
 end
