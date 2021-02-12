bf0d3f1a9e9e430ee234e7c5ba4507ecc00cfac6
tomomi ogino <s4tm83@gmail.com>
2021-02-12 15:53:09 +0900

    add: ユーザー新規作成機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,39 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  def index
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user), flash: {success: t('flash.success.user.create', user: @user.name)}
+    else
+      flash.now[:danger] = t('flash.alert.user.create')
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  def update
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_comment, :image, :image_cache)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
