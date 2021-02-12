64af699b0dee725e0c192d45cb996872ede1c13b
tomomi ogino <s4tm83@gmail.com>
2021-02-12 21:38:44 +0900

    add: artistsコントローラー作成、ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users, except: [:destroy]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :artists, only: [:index, :new, :create, :show]
 end
