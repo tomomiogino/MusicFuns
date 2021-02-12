9674775756c0e56276ad9ab4ac9df05c78114fa0
tomomi ogino <s4tm83@gmail.com>
2021-02-12 19:26:57 +0900

    add: ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+  def authenticate_user
+    redirect_to new_session_path, flash: {danger: t('flash.alert.session.login is needed')} unless logged_in?
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: session_params[:email].downcase)
+    if user && user.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id), flash: {success: t('flash.success.session.create')}
+    else
+      flash.now[:danger] = t('flash.alert.session.create')
+      render :new
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
