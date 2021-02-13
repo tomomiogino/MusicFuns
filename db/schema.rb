d4d32acd010e51a03fa0d1ebb16190512664979c
tomomi ogino <s4tm83@gmail.com>
2021-02-13 20:06:50 +0900

    Fansテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_094841) do
+ActiveRecord::Schema.define(version: 2021_02_13_110406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2021_02_13_094841) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["artist_id"], name: "index_channels_on_artist_id"
+  end
+
+  create_table "fans", force: :cascade do |t|
+    t.integer "artist_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
