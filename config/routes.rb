a22f10cd7ad501e1e3b96bcea59d5e66468a4902
tomomi ogino <s4tm83@gmail.com>
2021-02-13 22:41:29 +0900

    add: Relationshipsコントローラー作成、ルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, except: [:destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :fans, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   resources :artists, only: [:index, :new, :create, :show] do
     resources :channels
   end
