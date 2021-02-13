e870778a1bbb32a6034a0cfa87f4f4c18c73d56d
tomomi ogino <s4tm83@gmail.com>
2021-02-13 19:01:19 +0900

    update: ユーザー新規登録と同時にログインさせるように改善

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user), flash: {success: t('flash.success.user.create', user: @user.name)}
     else
       flash.now[:danger] = t('flash.alert.user.create')
