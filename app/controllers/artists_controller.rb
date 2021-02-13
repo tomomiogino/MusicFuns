9497663cfbf7b3651d17ecfa298d11b9675240a8
tomomi ogino <s4tm83@gmail.com>
2021-02-13 18:00:39 +0900

    add: artists一覧、新規作成機能実装

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,6 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show]
+
   require 'rspotify'
   RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
 
@@ -10,8 +12,32 @@ class ArtistsController < ApplicationController
   end
 
   def new
+    @artist = Artist.new(name: params[:name])
+    @image = params[:image]
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if params[:image]
+      @artist.remote_image_url = params[:image]
+    end
+    if @artist.save
+      redirect_to artist_path(@artist.id), flash: {success: "#{@artist.name}のアーティストページを作成しました！"}
+    else
+      flash.now[:danger] = "アーティストページの作成に失敗しました"
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+  def artist_params
+    params.require(:artist).permit(:name, :image, :image_cache)
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
   end
 end
