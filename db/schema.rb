c9088c715d20e0dfd5fa86cc5b6410f4db2ab455
tomomi ogino <s4tm83@gmail.com>
2021-02-14 17:42:36 +0900

    add: commentsテーブル作成、バリデーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_133101) do
+ActiveRecord::Schema.define(version: 2021_02_14_083910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2021_02_13_133101) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["artist_id"], name: "index_channels_on_artist_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content", null: false
+    t.bigint "channel_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["channel_id"], name: "index_comments_on_channel_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "fans", force: :cascade do |t|
@@ -59,4 +69,6 @@ ActiveRecord::Schema.define(version: 2021_02_13_133101) do
   end
 
   add_foreign_key "channels", "artists"
+  add_foreign_key "comments", "channels"
+  add_foreign_key "comments", "users"
 end
