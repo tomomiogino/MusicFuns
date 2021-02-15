ddec744fa38692c162ef49c453ab4229ff1a2f8e
tomomi ogino <s4tm83@gmail.com>
2021-02-15 18:01:19 +0900

    fix: ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'tops/index'
+  root 'tops#index'
   resources :users, except: [:destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :fans, only: [:create, :destroy]
