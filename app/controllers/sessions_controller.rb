276c537388b41aa763d51fecd873c9267e29d334
tomomi ogino <s4tm83@gmail.com>
2021-02-18 18:37:10 +0900

    update: 画面遷移修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email].downcase)
     if user && user.authenticate(session_params[:password])
       session[:user_id] = user.id
-      redirect_to user_path(user.id), flash: {success: t('flash.success.session.create')}
+      redirect_to artists_path, flash: {success: t('flash.success.session.create')}
     else
       flash.now[:danger] = t('flash.alert.session.create')
       render :new
