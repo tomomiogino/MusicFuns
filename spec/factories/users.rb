adea7a12ccb9ff1180d1d540e9557b2332908163
tomomi ogino <s4tm83@gmail.com>
2021-02-20 20:22:38 +0900

    add: userのModelSpec実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,17 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
-    password_digest { "MyString" }
-    profile_comment { "MyText" }
-    image { "MyText" }
+    name { "テストユーザー1" }
+    email { "test1@example.com" }
+    password { "123456" }
+    profile_comment { "テストコメント1" }
+    image { open("#{Rails.root}/public/seed_img/user_1.png") }
+  end
+
+  factory :second_user, class: User do
+    name { "テストユーザー2" }
+    email { "test2@example.com" }
+    password { "123456" }
+    profile_comment { "よろしく！" }
+    image { open("#{Rails.root}/public/seed_img/user_2.png") }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+
+  describe 'バリデーションのテスト' do
+    context '名前が空の場合' do
+      it 'バリデーションにひっかる' do
+        user.name = ''
+        expect(user).not_to be_valid
+      end
+    end
+    context '名前が20文字を超える長さの場合' do
+      it 'バリデーションにひっかかる' do
+        user.name = 'a' * 21
+        expect(user).not_to be_valid
+      end
+    end
+    context 'メールアドレスが空の場合' do
+      it 'バリデーションにひっかかる' do
+        user.email = ''
+        expect(user).not_to be_valid
+      end
+    end
+    context 'メールアドレスが255文字を超える長さの場合' do
+      it 'バリデーションにひっかかる' do
+        user.email = "#{'a' * 244}@example.com"
+        test_user = User.new(name: 'a' * 20, email: "#{'a' * 243}@example.com", password: '123456')
+        expect(user).not_to be_valid
+        expect(test_user).to be_valid
+      end
+    end
+    context 'メールアドレスのフォーマットが正しくない場合' do
+      it 'バリデーションにひっかかる' do
+        user.email = 'email'
+        expect(user).not_to be_valid
+      end
+    end
+    context '重複したメールアドレスの場合' do
+      it 'バリデーションにひっかかる' do
+        test_user = User.new(name: 'a' * 20, email: 'test1@example.com', password: '123456')
+        expect(test_user).not_to be_valid
+      end
+    end
+    context 'メールアドレスに大文字が含まれている場合' do
+      it '小文字で保存される' do
+        test_user = User.new(name: 'a' * 20, email: 'TEST1@example.com', password: '123456')
+        expect(user.email).to eq 'test1@example.com'
+      end
+    end
+    context 'インスタンス作成時、パスワードが空の場合' do
+      it 'バリデーションにひっかかる' do
+        test_user = User.new(name: 'a' * 20, email: 'test@example.com', password: '')
+        expect(test_user).not_to be_valid
+      end
+    end
+    context 'インスタンス作成時、パスワードが6文字未満の場合' do
+      it 'バリデーションにひっかかる' do
+        test_user = User.new(name: 'a' * 20, email: 'test@example.com', password: '12345')
+        expect(test_user).not_to be_valid
+      end
+    end
+    context 'プロフィールコメントが255文字を超える長さの場合' do
+      it 'バリデーションにひっかかる' do
+        user.profile_comment = 'a' * 256
+        expect(user).not_to be_valid
+      end
+    end
+    context '名前が20文字以内で記載されている、且つ重複しないメールアドレスが正しいフォーマットで255文字以内で記載されている、且つパスワードが6文字以上で記載されている場合' do
+      it 'バリデーションが通る' do
+        user = User.new(name: 'a' * 20, email: 'test@example.com', password: '123456')
+        expect(user).to be_valid
+      end
+    end
+  end
 end
